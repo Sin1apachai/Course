0ba84d87962e7bcb0480cb54b195d7d4fe553678
Chairoj/Sinlapachai <40155594+Sin1apachai@users.noreply.github.com>
2024-03-22 21:01:58 +0700

    Create operators.py

---
+++ Exercise/operators.py
@@ -0,0 +1,13 @@
+## use operator + to find the result of 10 + 20
+
+## use operator - to find the result of 10 - 20
+
+## use operator - to find the result of 20 - 10
+
+## use operator * to find the result of 10 + 2
+
+## use operator / to find the result of 10 / 2
+
+## use operator % to find the result of 10 % 2
+
+## use operator // to find the result of 10 // 2
