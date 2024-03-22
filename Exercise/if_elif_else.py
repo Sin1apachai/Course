97590b7192aa1592d23733b853cc6d537a6f3e1f
Chairoj/Sinlapachai <40155594+Sin1apachai@users.noreply.github.com>
2024-03-22 21:08:30 +0700

    Create if_elif_else.py

---
+++ Exercise/if_elif_else.py
@@ -0,0 +1,3 @@
+## Create a condition using if else. The conditions are as follows.
+## Let a and b be equal to 10 and 20, respectively. If a is equal to b, then "a=b" is expressed, and if a is equal to b * 2, then "a is equal to b*2". 
+## Finally, if none of the conditions are met, it will be shown that "Nothing is correct."
