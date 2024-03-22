388f5e76ab6ea026009409777d3a738ce2be1be7
Chairoj/Sinlapachai <40155594+Sin1apachai@users.noreply.github.com>
2024-03-22 21:13:58 +0700

    Create function.py

---
+++ Exercise/function.py
@@ -0,0 +1,4 @@
+## Create a function with the following operations.
+## Create the variable x as a global variable with a value of 10. Create a function named evennumber.
+## This function checks if the value of the variable x is an even number or not. If the value is an even number, 
+## it will return the value "x is an even number" and if it is an odd number, it will return the value x and display the message "x is an odd number".
