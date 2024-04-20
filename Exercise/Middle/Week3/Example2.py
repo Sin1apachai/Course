dbb178c31e22ade0f2ed735a091543b386e0f150
Chairoj/Sinlapachai <40155594+Sin1apachai@users.noreply.github.com>
2024-04-20 14:06:19 +0700

    Create Example2.py

---
+++ Exercise/Middle/Week3/Example2.py
@@ -0,0 +1,21 @@
+############
+### ENG ####
+############
+# Create a program to receive registration information of coffee drinkers. Within, include:
+# 1.Coffee name
+# 2.Price
+# 3.Drinker
+# Allow users to input coffee information without limitations and display the received data as follows:
+# Result
+# We have received your coffee. "Drinker's name" your drink "Coffee name" (Price "Price")
+
+################
+##### THAI #####
+################
+# สร้างโปรแกรมสำหรับรับข้อมูลการลงทะเบียนของผู้ดื่มกาแฟ โดยภายในประกอบไปด้วย
+# 1. ชื่อกาแฟ
+# 3. ราคา
+# 4. ผู้ดื่ม
+# โดยให้ผู้ใช้สามารถกรอกข้อมูลของกาแฟได้ไม่จำกัดและแสดงผลการรับข้อมูลดังนี้
+# Result
+# We have recieve your coffee. "ชื่อผู้ดื่ม" your drink "ชื่อกาแฟ" (Price "ราคา")
